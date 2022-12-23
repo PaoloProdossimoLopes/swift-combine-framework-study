02accba9425ddec75cac219d673f05f48ca0408c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-12-22 21:11:11 -0300

    chore: criando o projeto inicial para testes do UIKit

---
+++ combine-with-uikit/combine-with-uikit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  combine-with-uikit
+//
+//  Created by Paolo Prodossimo Lopes on 22/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ combine-with-uikit/combine-with-uikitUITests/combine_with_uikitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  combine_with_uikitUITestsLaunchTests.swift
+//  combine-with-uikitUITests
+//
+//  Created by Paolo Prodossimo Lopes on 22/12/22.
+//
+
+import XCTest
+
+final class combine_with_uikitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
